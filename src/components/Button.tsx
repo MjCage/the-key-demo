b0582922643cb79310fe0bca48d027a89be4479d
MjCage <michael@cage-dev.de>
2024-01-16 15:50:26 +0100

    feat: setup form validation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({ label, className, ...props }: ButtonProps) => {
 	return (
 		<button
-			className={`flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className ?? ""}`}
+			className={`flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 disabled:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className ?? ""}`}
 			{...props}
 		>
 			{label}
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,46 +1,73 @@
 import React from "react";
 import { Button } from "./Button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, string } from "yup";
+
+type Inputs = {
+	email: string;
+	password: string;
+};
+
+const schema = object({
+	email: string()
+		.required("Email is required to login.")
+		.email("Invalid email address."),
+	password: string().required("Password is required to authenticate."),
+}).required();
 
 interface LoginFormProps {
 	className?: string;
 }
 
 export const LoginForm = ({ className }: LoginFormProps) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+	} = useForm<Inputs>({
+		resolver: yupResolver(schema),
+		mode: "onTouched",
+	});
+
+	const onSubmit = () => {
+		// ToDo: Trigger login
+		console.log("Fired");
+	};
+
 	return (
 		<form
-			className={`w-full p-4 space-y-6 bg-white rounded-xl drop-shadow-md ${className ?? ""}`}
-			action="#"
-			method="POST"
+			className={`w-full p-4 bg-white rounded-xl drop-shadow-md ${className ?? ""}`}
+			onSubmit={handleSubmit(onSubmit)}
 		>
 			<label
 				htmlFor="email"
-				className="block text-sm font-medium leading-6 text-gray-900"
+				className="block mb-2 text-sm font-medium leading-6 text-gray-900"
 			>
 				Email address
 			</label>
 			<input
-				id="email"
-				name="email"
-				type="email"
 				autoComplete="email"
-				required
-				className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				className="block mb-1 w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				{...register("email", { required: true })}
 			/>
+			<p className="text-red-500 text-xs h-2">{errors.email?.message}</p>
 			<label
 				htmlFor="password"
-				className="block text-sm font-medium leading-6 text-gray-900"
+				className="block mb-2 mt-6 text-sm font-medium leading-6 text-gray-900"
 			>
 				Password
 			</label>
 			<input
-				id="password"
-				name="password"
 				type="password"
 				autoComplete="current-password"
-				required
-				className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				className="block mb-1 w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				{...register("password", { required: true })}
 			/>
-			<Button type="submit" label="Sign in" />
+			<p className="text-red-500 text-xs mb-8 h-2">
+				{errors.password?.message}
+			</p>
+			<Button type="submit" label="Sign in" disabled={!isValid} />
 		</form>
 	);
 };
