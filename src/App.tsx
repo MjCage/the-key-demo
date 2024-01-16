3d5bcad407523bbe52732763e63a6e72addeffa8
MjCage <michael@cage-dev.de>
2024-01-16 10:05:17 +0100

    feat: setup prettier and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 function App() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      the key demo
-    </div>
-  );
+	return (
+		<div className="w-full h-screen flex justify-center items-center">
+			the key demo
+		</div>
+	);
 }
 
 export default App;
