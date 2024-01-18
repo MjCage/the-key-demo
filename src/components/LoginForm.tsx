2d92aaa7b109f15cd9ac415ad223ef3613ca83ce
MjCage <michael@cage-dev.de>
2024-01-18 20:16:00 +0100

    fix: label issues

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -71,6 +71,7 @@ export const LoginForm = ({ className }: LoginFormProps) => {
 				E-Mail Adresse
 			</label>
 			<input
+				id="email"
 				autoComplete="email"
 				className="block mb-1 w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 				{...register("email", { required: true })}
@@ -83,6 +84,7 @@ export const LoginForm = ({ className }: LoginFormProps) => {
 				Passwort
 			</label>
 			<input
+				id="password"
 				type="password"
 				autoComplete="current-password"
 				className="block mb-1 w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
